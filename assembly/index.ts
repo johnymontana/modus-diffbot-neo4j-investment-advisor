239198cc0acd7549cbca4d38787244dc0178c532
Michael Hunger <github@jexp.de>
2024-11-17 10:30:44 -0800

    trying to get the embedding search to work, but stackoverflow

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,4 +1,4 @@
-export { getCompany  } from "./query_graph";
+export { getCompany, getNews, embedText, generateText  } from "./query_graph";
 
 export function sayHello(name: string | null = null): string {
   return `Hello, ${name || "World"}!`;
--- assembly/query_graph.ts
+++ assembly/query_graph.ts
@@ -28,7 +28,7 @@ class Statement {
     parameters: Map<string, string> = new Map<string, string>();
 }
 
-function query_database<T>(query:String, parameters:Map<string,string>, cb:(values:string[])=>T): T[] {
+function query_database(query:String, parameters:Map<string,string>): Result {
     const request = new http.Request("https://diffbot.neo4jlabs.com:7473/db/neo4j/query/v2", 
     {method: "POST", 
      headers: http.Headers.from([[ "Content-Type", "application/json" ]]),
@@ -36,7 +36,7 @@ function query_database<T>(query:String, parameters:Map<string,string>, cb:(valu
     } as http.RequestOptions);
 
 
-  console.log(String.UTF8.decode(request.body));
+  console.log(String.UTF8.decode(request.body));    
   const response = http.fetch(request);
   if (!response.ok) {
     throw new Error(
@@ -47,19 +47,46 @@ function query_database<T>(query:String, parameters:Map<string,string>, cb:(valu
   // the API returns an array of quotes, but we only want the first one
   const result = response.json<Result>();
   console.log(JSON.stringify(result));
-  return result.data.values.map<T>((values:string[]) => cb(values));
+  return result;
 }
 // this function makes a request to an API that returns data in JSON format, and
 // returns an object representing the data
 export function getCompany(name: string): Company[] {
     // call db.index.fulltext.queryNodes('entity',$name, {limit:20}) yield node as c
     const query = `
-        MATCH (c:Organization) RETURN c.id as id, c.name as name LIMIT 1
+        MATCH (c:Organization) WHERE c.name = $name RETURN c.id as id, c.name as name LIMIT 1
     `;
     const parameters = new Map<string, string>();
     parameters.set("name", name);
-    return query_database<Company>(query, parameters, (value:string[]) =>  {return {id: value[0], name: value[1]} as Company});
+    const result = query_database(query, parameters);
+    return result.data.values.map<Company>((values:string[]) => {return {id: values[0], name: values[1]} as Company});
 }
+
+@json
+class Article {
+  id!: string;
+  title!: string;
+  author!: string;
+  score!: number;
+}
+
+export function getNews(text: string): Article[] {
+    // call db.index.fulltext.queryNodes('entity',$name, {limit:20}) yield node as c
+    const embedding = embedText(text);
+//    console.log(JSON.stringify(embedding));
+    const query = `
+        call db.index.vector.queryNodes('news', 5, apoc.convert.fromJsonList($embedding)) yield node as c, score
+        match (c)<-[:HAS_CHUNK]-(a)
+        RETURN a.id as id, a.title as title, a.author as author, toString(score)
+    `;
+    console.log(query);
+    const parameters = new Map<string, string>();
+    parameters.set("embedding", JSON.stringify(embedding));
+    const result = query_database(query, parameters);
+    console.log(JSON.stringify(result));
+    return result.data.values.map<Article>((values:string[]) => {return {id: values[0], title: values[1], author: values[2], score: parseFloat(values[3])} as Article});
+}
+
 
 import { models } from "@hypermode/modus-sdk-as"
 import {
@@ -89,11 +116,11 @@ export function generateText(instruction: string, prompt: string): string {
   return output.choices[0].message.content.trim()
 }
 
-export function embedText(text:string): Embedding {
+export function embedText(text:string): f32[] {
     const model = models.getModel<OpenAIEmbeddingsModel>("text-embedder")
     const input = model.createInput(text);
   
     const output = model.invoke(input)
-    return output.data[0];
+    return output.data[0].embedding;
 }
   
